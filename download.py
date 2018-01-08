9be0382f7eccb65ea49303aee2f2c341ce6704c5
“David <david.read@hackneyworkshop.com>
2018-01-08 18:03:15 +0000

    Add upload to S3

--- download.py
+++ download.py
@@ -9,16 +9,18 @@ import requests
 def download(url, id_='abc', extension='xls'):
     filename = url2filename(url)
     filepath = apply_filepath_conventions('dgu_data', filename, id_, extension)
-    download_file(url, filepath)
+    response = download_file(url, filepath)
     print('Downloaded: {}'.format(filepath))
+    return filepath, response
 
 
 def download_file(url, filename):
-    r = requests.get(url, stream=True)  # stream it to disk
+    response = requests_wrapper(requests.get, url, stream=True)  # stream it to disk
     with open(filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024): 
+        for chunk in response.iter_content(chunk_size=1024): 
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
+    return response
 
 
 def url2filename(url):
@@ -36,3 +38,33 @@ def url2filename(url):
 def apply_filepath_conventions(path, basename, id, extension):
     filename = '{}-{}.{}'.format(basename, id, extension)
     return os.path.join(path, filename)
+
+
+class DownloadException(Exception):
+    pass
+
+def requests_wrapper(func, *args, **kwargs):
+    '''
+    Run a requests command, catching any exceptions and reraising them as
+    DownloadException. Status errors, such as 404 or 500 do not cause
+    exceptions. Instead they are exposed as not response.ok.
+    e.g.
+    >>> requests_wrapper(requests.get, url, timeout=url_timeout)
+    runs:
+        res = requests.get(url, timeout=url_timeout)
+    '''
+    try:
+        response = func(*args, **kwargs)
+    except requests.exceptions.ConnectionError as e:
+        raise DownloadException('Connection error: {}'.format(e))
+    except requests.exceptions.HTTPError as e:
+        raise DownloadException('Invalid HTTP response: {}'.format(e))
+    except requests.exceptions.Timeout as e:
+        raise DownloadException('Connection timed out after {}'.format(kwargs.get('timeout', '?')))
+    except requests.exceptions.TooManyRedirects as e:
+        raise DownloadException('Too many redirects')
+    except requests.exceptions.RequestException as e:
+        raise DownloadException('Error downloading: {}'.format(e))
+    except Exception as e:
+        raise DownloadException('Error with the download: {}'.format(e))
+    return response
--- download_dgu.py
+++ download_dgu.py
@@ -5,37 +5,93 @@ Downloads data.gov.uk Excel files
 import argparse
 import json
 
-from download import download
+import boto3
+
+from download import download, requests_wrapper, DownloadException
 from loopstats import LoopStats
 
 
-def process_jsonl(dgu_jsonl_filepath):
-    loopstats = LoopStats()
+S3_BUCKET_NAME = 'buod-spreadsheetclustering-dgu'
+
+def process_jsonl(dgu_jsonl_filepath, limit, dataset_filter, res_filter, dont_download, dont_upload):
+    res_stats = LoopStats()
+    dataset_stats = LoopStats()
+
     with open(dgu_jsonl_filepath, 'r') as f:
         for line in f:
             dataset = json.loads(line)
+
+            if dataset_filter and dataset['name'] != dataset_filter:
+                continue
+
+            dataset_has_xls = False
             for resource in dataset['resources']:
                 res_identifier = '{}/resource/{}'.format(dataset['name'], resource['id'])
+                print(res_identifier)
+                if res_filter and res_filter not in (res_identifier, resource['id']):
+                    continue
                 if resource.get('format') == 'XLS':
-                    print(loopstats.add('format=XLS', res_identifier))
+                    print(res_stats.add('format=XLS', res_identifier))
+                    dataset_has_xls = True
                 elif resource.get('qa', {}).get('format') == 'XLS':
-                    print(loopstats.add('QA.format=XLS', res_identifier))
+                    print(res_stats.add('QA.format=XLS', res_identifier))
+                    if not dataset_has_xls:
+                        dataset_has_xls = 'Has XLS by falling back to QA'
                 else:
-                    loopstats.add('rejected format={}'.format(resource.get('format')), res_identifier)
+                    res_stats.add('rejected format={}'.format(resource.get('format')), res_identifier)
                     continue
                 
-                res_identifier = '{}-{}'.format(dataset['name'], resource['id'][:4])
-                download(resource['url'], id_=res_identifier, extension='xls')
-        loopstats.print_every_x_iterations(100)
-        
-    print('\nStats:\n', loopstats)
+                if dont_download:
+                    break
+
+                # download
+                res_filename_identifier = '{}-{}'.format(dataset['name'], resource['id'][:4])
+                try:
+                    filepath, response = download(
+                        resource['url'], id_=res_filename_identifier, extension='xls')
+                except DownloadException as e:
+                    error_type = str(e).split(':')[0]
+                    error_details = str(e)[len(error_type):]
+                    print(res_stats.add('Download error: {}'.format(error_type), '{} - {}'.format(error_details, res_identifier)))
+                    continue
+
+                if dont_upload:
+                    break
+
+                # upload to S3
+                s3 = boto3.client('s3')
+                s3.upload_file(filepath, S3_BUCKET_NAME, res_identifier)
+                print(res_stats.add('uploaded ok', res_identifier))
+
+            if dataset_has_xls is True:
+                dataset_stats.add('Has XLS', dataset['name'])
+            elif dataset_has_xls is False:
+                dataset_stats.add('No XLS', dataset['name'])
+            else:
+                dataset_stats.add(dataset_has_xls, dataset['name'])
+            res_stats.print_every_x_iterations(1000)
+            dataset_stats.print_every_x_iterations(1000)
+            if limit and dataset_stats.count >= limit:
+                print('Stopping now the limit {} is reached'.format(limit))
+                break
+
+    print('\nResource Stats:\n', res_stats)
+    print('\nDataset Stats:\n', dataset_stats)
     #pool = multiprocessing.Pool(processes=4)
     #pool.map(process_url, urls)
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--dgu-jsonl-filepath', default='dgu.jsonl.sample')
+    parser.add_argument('-f', '--dgu-jsonl-filepath', default='dgu.jsonl.sample')
+    parser.add_argument('--limit', type=int, help='Stop after a given number of datasets')
+    parser.add_argument('-d', '--dataset-filter', default=None, help='Filter to a particular dataset ID')
+    parser.add_argument('-r', '--res-filter', default=None, help='Filter to a particular resource ID')
+    parser.add_argument('--dont-download', action='store_true', help='Stop after identifying the resource (no download or upload to S3)')
+    parser.add_argument('--dont-upload', action='store_true', help='Stop after downloading the data (no upload to S3)')
     args = parser.parse_args()
-    process_jsonl(args.dgu_jsonl_filepath)
-    
+
+    if not args.dataset_filter and args.res_filter and '/' in args.res_filter:
+        args.dataset_filter = args.res_filter.split('/')[0]
+
+    process_jsonl(args.dgu_jsonl_filepath, args.limit, args.dataset_filter, args.res_filter, args.dont_download, args.dont_upload)
